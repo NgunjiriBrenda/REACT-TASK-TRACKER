333d7697c694b2a799010dc647318b00048b4141
brenda njoki <ngunjiribrenda5@gmail.com>
2026-06-25 19:40:39 +0300

    Creates faker data for the tasks and loops into the lsik and loops through

--- task-tracker/src/App.jsx
+++ task-tracker/src/App.jsx
@@ -1,3 +1,28 @@
 function App(){
 
-}const tasks
+
+//Fake data for tasks-array of objects
+const tasks=[
+  {id: 1, text: "Learn React basics", completed: false},
+  {id: 2, text: "Build a simple app", completed: false},
+  {id: 3, text: "Learn Database Systems", completed: false},
+  {id: 4, text: "Learn Props and Components", completed: true},
+];
+
+return (
+  <div className="app">
+    <h1>My Task Tracker</h1>
+
+    <ul> 
+      {tasks.map((task) => (
+        <li key={task.id}>
+          {task.text} {task.completed? "":<button>Mark as Completed</button>}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+}
+
+export default App;
